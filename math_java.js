aa304e43833a0603e82241b208b8c46dcf9caf0f
user3126 <127672506+user3126@users.noreply.github.com>
2024-11-01 15:41:03 +0900

    Add files via upload

---
+++ math_java.js
@@ -0,0 +1,228 @@
+var myimg1 = document.getElementById("answer1");
+var mybutton1 = document.getElementById("button1");
+
+myimg1.style.display = "none";
+
+mybutton1.addEventListener("click", event => {
+
+    if (myimg1.style.display === "none") {
+        myimg1.style.display = "block";
+        mybutton1.textContent = "정답 가리기";
+    }
+
+    else {
+        myimg1.style.display = "none";
+        mybutton1.textContent = "정답 보기";
+
+    }
+});
+
+var myimg2 = document.getElementById("answer2");
+var mybutton2 = document.getElementById("button2");
+
+myimg2.style.display = "none";
+
+mybutton2.addEventListener("click", (event) => {
+  if (myimg2.style.display === "none") {
+    myimg2.style.display = "block";
+    mybutton2.textContent = "정답 가리기";
+  } else {
+    myimg2.style.display = "none";
+    mybutton2.textContent = "정답 보기";
+  }
+});
+
+var myimg3 = document.getElementById("answer3");
+var mybutton3 = document.getElementById("button3");
+
+myimg3.style.display = "none";
+
+mybutton3.addEventListener("click", (event) => {
+  if (myimg3.style.display === "none") {
+    myimg3.style.display = "block";
+    mybutton3.textContent = "정답 가리기";
+  } else {
+    myimg3.style.display = "none";
+    mybutton3.textContent = "정답 보기";
+  }
+});
+
+var myimg4 = document.getElementById("answer4");
+var mybutton4 = document.getElementById("button4");
+
+myimg4.style.display = "none";
+
+mybutton4.addEventListener("click", (event) => {
+  if (myimg4.style.display === "none") {
+    myimg4.style.display = "block";
+    mybutton4.textContent = "정답 가리기";
+  } else {
+    myimg4.style.display = "none";
+    mybutton4.textContent = "정답 보기";
+  }
+});
+
+var myimg5 = document.getElementById("answer5");
+var mybutton5 = document.getElementById("button5");
+
+myimg5.style.display = "none";
+
+mybutton5.addEventListener("click", (event) => {
+  if (myimg5.style.display === "none") {
+    myimg5.style.display = "block";
+    mybutton5.textContent = "정답 가리기";
+  } else {
+    myimg5.style.display = "none";
+    mybutton5.textContent = "정답 보기";
+  }
+});
+
+var myimg6 = document.getElementById("answer6");
+var mybutton6 = document.getElementById("button6");
+
+myimg6.style.display = "none";
+
+mybutton6.addEventListener("click", (event) => {
+  if (myimg6.style.display === "none") {
+    myimg6.style.display = "block";
+    mybutton6.textContent = "정답 가리기";
+  } else {
+    myimg6.style.display = "none";
+    mybutton6.textContent = "정답 보기";
+  }
+});
+
+var myimg7 = document.getElementById("answer7");
+var mybutton7 = document.getElementById("button7");
+
+myimg7.style.display = "none";
+
+mybutton7.addEventListener("click", (event) => {
+  if (myimg7.style.display === "none") {
+    myimg7.style.display = "block";
+    mybutton7.textContent = "정답 가리기";
+  } else {
+    myimg7.style.display = "none";
+    mybutton7.textContent = "정답 보기";
+  }
+});
+
+var myimg8 = document.getElementById("answer8");
+var mybutton8 = document.getElementById("button8");
+
+myimg8.style.display = "none";
+
+mybutton8.addEventListener("click", (event) => {
+  if (myimg8.style.display === "none") {
+    myimg8.style.display = "block";
+    mybutton8.textContent = "정답 가리기";
+  } else {
+    myimg8.style.display = "none";
+    mybutton8.textContent = "정답 보기";
+  }
+});
+
+var myimg9 = document.getElementById("answer9");
+var mybutton9 = document.getElementById("button9");
+
+myimg9.style.display = "none";
+
+mybutton9.addEventListener("click", (event) => {
+  if (myimg9.style.display === "none") {
+    myimg9.style.display = "block";
+    mybutton9.textContent = "정답 가리기";
+  } else {
+    myimg9.style.display = "none";
+    mybutton9.textContent = "정답 보기";
+  }
+});
+
+var myimg10 = document.getElementById("answer10");
+var mybutton10 = document.getElementById("button10");
+
+myimg10.style.display = "none";
+
+mybutton10.addEventListener("click", (event) => {
+  if (myimg10.style.display === "none") {
+    myimg10.style.display = "block";
+    mybutton10.textContent = "정답 가리기";
+  } else {
+    myimg10.style.display = "none";
+    mybutton10.textContent = "정답 보기";
+  }
+});
+
+var myimg11 = document.getElementById("answer11");
+var mybutton11 = document.getElementById("button11");
+
+myimg11.style.display = "none";
+
+mybutton11.addEventListener("click", (event) => {
+  if (myimg11.style.display === "none") {
+    myimg11.style.display = "block";
+    mybutton11.textContent = "정답 가리기";
+  } else {
+    myimg11.style.display = "none";
+    mybutton11.textContent = "정답 보기";
+  }
+});
+
+var myimg12 = document.getElementById("answer12");
+var mybutton12 = document.getElementById("button12");
+
+myimg12.style.display = "none";
+
+mybutton12.addEventListener("click", (event) => {
+  if (myimg12.style.display === "none") {
+    myimg12.style.display = "block";
+    mybutton12.textContent = "정답 가리기";
+  } else {
+    myimg12.style.display = "none";
+    mybutton12.textContent = "정답 보기";
+  }
+});
+
+var myimg13 = document.getElementById("answer13");
+var mybutton13 = document.getElementById("button13");
+
+myimg13.style.display = "none";
+
+mybutton13.addEventListener("click", (event) => {
+  if (myimg13.style.display === "none") {
+    myimg13.style.display = "block";
+    mybutton13.textContent = "정답 가리기";
+  } else {
+    myimg13.style.display = "none";
+    mybutton13.textContent = "정답 보기";
+  }
+});
+
+var myimg14 = document.getElementById("answer14");
+var mybutton14 = document.getElementById("button14");
+
+myimg14.style.display = "none";
+
+mybutton14.addEventListener("click", (event) => {
+  if (myimg14.style.display === "none") {
+    myimg14.style.display = "block";
+    mybutton14.textContent = "정답 가리기";
+  } else {
+    myimg14.style.display = "none";
+    mybutton14.textContent = "정답 보기";
+  }
+});
+
+var myimg15 = document.getElementById("answer15");
+var mybutton15 = document.getElementById("button15");
+
+myimg15.style.display = "none";
+
+mybutton15.addEventListener("click", (event) => {
+  if (myimg15.style.display === "none") {
+    myimg15.style.display = "block";
+    mybutton15.textContent = "정답 가리기";
+  } else {
+    myimg15.style.display = "none";
+    mybutton15.textContent = "정답 보기";
+  }
+});
